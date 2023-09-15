57c3198fc38fce233bad8e58217c672006c1e7c3
Avi Gewirtz <sgewirtz@princeton.edu>
2023-09-15 14:55:50 -0400

    styling and other important functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';  // Importing layout components from Ant Design
 import HomePage from './pages/HomePage'; // or './components/HomePage'
 import './App.css';
 
+const { Header, Content, Footer } = Layout;  // Destructuring layout components
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          {/* other routes can be added below */}
-        </Routes>
-      </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header style={{ color: 'white', textAlign: 'center', fontSize: '24px' }}>
+          App Name  {/* This can be the name or logo of your app */}
+        </Header>
+        <Content style={{ padding: '20px 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/* other routes can be added below */}
+            </Routes>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Footer Information</Footer> {/* This can be a footer text or copyright information */}
+      </Layout>
     </Router>
   );
 }
 
 export default App;
-
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './HomePage.css';
-// import { PDFDocument } from 'pdf-lib';
+import { Button, Input, Select, Spin, Alert, Typography, Card } from 'antd';
 import * as pdfjsLib from 'pdfjs-dist/webpack';
 import Tesseract from 'tesseract.js';
 
 function HomePage() {
-    const [notes, setNotes] = useState([]); // each item is { text: "", response: "" }
+    const { Option } = Select;
+    const { TextArea } = Input;
+    const { Title } = Typography;
+
+    const [notes, setNotes] = useState([]);
     const [selectedOption, setSelectedOption] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [responseText, setResponseText] = useState("");
 
     const extractAllTextFromPDF = async (dataUrl) => {
         const loadingTask = pdfjsLib.getDocument(dataUrl);
@@ -52,96 +56,114 @@ function HomePage() {
                 const reader = new FileReader();
                 reader.onload = async function(event) {
                     const extractedText = await extractAllTextFromPDF(event.target.result);
-                    resolve({ text: extractedText, response: "" });
+                    resolve(`--- Start of Note from ${file.name} ---\n${extractedText}\n`);
                 };
                 reader.onerror = reject;
                 reader.readAsDataURL(file);
             });
         });
-
+    
         Promise.all(newNotesPromises).then(notesArray => {
-            setNotes(prevNotes => [...prevNotes, ...notesArray]);
+            // Here's the change. We are directly setting the state to the new notes array
+            // instead of spreading the previous notes with the new ones.
+            setNotes(notesArray);
             setIsLoading(false);
         }).catch(error => {
             setError("There was an error processing your files. Please ensure they are in the correct format and try again.");
             setIsLoading(false);
         });
     }
+    
 
-    const handleOptionChange = (e) => {
-        setSelectedOption(e.target.value);
-    }
-
-    const handleNoteChange = (e, index) => {
-        const updatedNotes = [...notes];
-        updatedNotes[index].text = e.target.value;
-        setNotes(updatedNotes);
+    const handleOptionChange = (value) => {
+        setSelectedOption(value);
     }
 
     const handleSubmit = async () => {
         setIsLoading(true);
         setError(null);
-
+    
         if (notes.length === 0 || !selectedOption) {
             alert("Please upload files and select an option.");
             setIsLoading(false);
             return;
         }
-
+    
         const data = {
             notes,
             choice: selectedOption
         };
-
-        const response = await axios.post('https://billing-automater-801d93465a2c.herokuapp.com/get-prompt', data, {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            withCredentials: true
-        });
-
-        setNotes(response.data.processedNotes);
+    
+        try {
+            const response = await axios.post('https://billing-automater-801d93465a2c.herokuapp.com/get-prompt', data, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true
+            });
+    
+            const processedResponses = response.data.processedNotes;
+            const concatenatedResponses = processedResponses.map((item, index) => {
+                if (index % 2 === 0) {
+                    return ''; 
+                }
+                return `--- Response for Note ${(index + 1) / 2} ---\n${item}\n`; 
+            }).join('\n');
+            
+            setResponseText(concatenatedResponses);
+    
+        } catch (error) {
+            setError("There was an error processing your request. Please try again.");
+        }
+    
         setIsLoading(false);
     }
 
     return (
-        <div className="app">
-            {isLoading && <div className="spinner">Loading...</div>}
-            {error && <div className="error">{error}</div>}
+        <div>
+            {isLoading && <Spin size="large" />}
+            {error && <Alert message={error} type="error" showIcon />}
 
-            <main>
-                <h1>The Smartest Way to Elevate your Progress Notes</h1>
+            <Title style={{ marginTop: '20px', marginBottom: '20px' }}>The Smartest Way to Elevate your Progress Notes</Title>
 
-                {notes.map((note, index) => (
-                    <div key={index}>
-                        <label>Input Text:</label>
-                        <textarea rows="10" cols="50" value={note.text} onChange={e => handleNoteChange(e, index)} />
-                    </div>
-                ))}
 
-                <label>Or Upload PDF Document:</label>
-                <input type="file" accept=".pdf" onChange={handleFileChange} multiple />
+            <div>
+            <label style={{ marginBottom: '20px' }}>Input Text:</label>
 
-                <label>What information would you like?:</label>
-                <select onChange={handleOptionChange}>
-                    <option value="">--Choose an option--</option>
-                    <option value="1">Patient's iagnosis</option>
-                    <option value="2">ICD10 and CPT Codes</option>
-                    <option value="3">Medicare verbiage if the patient can benefit for physical therapy.</option>
-                    <option value="4">Spell chec of the progress note</option>
-                    <option value="5">Care plan</option>
-                </select>
+            <TextArea 
+    rows="10" 
+    value={notes.map((note, index) => `--- Note ${index + 1} ---\n${note}\n`).join('\n')}
+    readOnly
+    style={{ marginBottom: '20px' }} // Add margin below the TextArea
+/>
 
-                <button onClick={handleSubmit}>Submit</button>
-                <div className="response-section">
-                    <h2>Response:</h2>
-                    {notes.map((note, index) => (
-                        <div key={index} className="output-box">
-                            <p>{note.response}</p>
-                        </div>
-                    ))}
-                </div>
-            </main>
+
+            </div>
+
+            <label>Or Upload PDF Document:</label>
+            <Input type="file" accept=".pdf" onChange={handleFileChange} multiple style={{ marginBottom: '20px' }} />
+
+
+
+            <label>What information would you like?:</label>
+            <Select onChange={handleOptionChange} style={{ width: '100%' }}>
+                <Option value="">--Choose an option--</Option>
+                <Option value="1">Patient's diagnosis</Option>
+                <Option value="2">ICD10 and CPT Codes</Option>
+                <Option value="3">Medicare verbiage if the patient can benefit for physical therapy.</Option>
+                <Option value="4">Spell check of the progress note</Option>
+                <Option value="5">Care plan</Option>
+                <Option value="6">Medication discrepency</Option>
+            </Select>
+
+            <Button type="primary" onClick={handleSubmit} style={{ marginTop: '8px' }}>Submit</Button>
+
+
+            <Card title="Response:" bordered={true} style={{ marginTop: '20px' }}>
+    <pre style={{ whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>{responseText}</pre>
+</Card>
+
+
         </div>
     );
 }
